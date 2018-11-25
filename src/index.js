00a94819e3300b2ba564ff34e97f78763d15537b
Patrick Hogan <phogan110@live.com>
2018-11-25 11:10:17 -0700

    referenced `Comment`

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Comment from './components/Comment';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+    <Comment
+        author={{name: 'Jackson', avatarUrl: 'https://covalence.io/static/img/team/Jackson.jpg'}}
+        text="Hi, thanks for listening to me say COMPONENT so. many. times."
+    />, 
+    document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
