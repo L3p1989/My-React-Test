a6bf61057a0b40b9448e07081a659ed535321e61
Patrick Hogan <phogan110@live.com>
2018-11-27 13:22:56 -0700

    Created TestInput

---
+++ src/components/TestInput.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class TestInput extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: 'Jackson'
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                value={this.state.name}
+                onChange={(e) => {this.setState({name: e.target.value})}} />
+            </div>
+        );
+    }
+}
+
+export default TestInput;
