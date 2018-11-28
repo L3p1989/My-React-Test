352c49db4d64e8897dc44dc9e93798044d013e45
Patrick Hogan <phogan110@live.com>
2018-11-28 09:43:05 -0700

    changed `App` to a class; added input with new onInputChange method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Comment from './components/Comment';
 
-const App = () => {
-  let element = <h1>Welcome to React</h1>;
-  return element;
+let element = <h1>Welcome to React</h1>;
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: ''
+    }
+  }
+
+  onInputChange = (value) => {
+    this.setState({name: value})
+  }
+
+  render() {
+    return (<>
+      {element}
+      <input 
+        placeholder="What's your name?"
+        value={this.state.name}
+        onChange={(e) => this.onInputChange(e.target.value)} 
+      />
+      <Comment
+        author={{ name: this.state.name, avatarUrl: 'https://covalence.io/static/img/team/Jackson.jpg' }}
+        text="Hi, thanks for listening to me say COMPONENT so. many. times!"
+      />
+    </>);
+  }
 }
 
 export default App;
