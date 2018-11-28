7cde2b633a779103aca27a65844d50bcb9c52315
Patrick Hogan <phogan110@live.com>
2018-11-28 09:43:38 -0700

    rendered `App` instead of `Comment`

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Comment from './components/Comment';
 
 ReactDOM.render(
-    <Comment
-        author={{name: 'Jackson', avatarUrl: 'https://covalence.io/static/img/team/Jackson.jpg'}}
-        text="Hi, thanks for listening to me say COMPONENT so. many. times!"
-    />, 
+    <App />, 
     document.getElementById('root')
 );
 serviceWorker.register();
