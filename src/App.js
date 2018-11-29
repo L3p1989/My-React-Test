000e4dc3b1e79192ba21324647aa35afbb7eb523
Patrick Hogan <phogan110@live.com>
2018-11-29 12:06:57 -0700

    added `List` Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Comment from './components/Comment';
+import List from './components/List';
 
 let element = <h1>Welcome to React</h1>;
 
@@ -12,6 +13,11 @@ class App extends Component {
     this.state = {
       name: ''
     }
+    this.items = [
+      {value: 'Some arbitrary value goes here', id: 0},
+      {value: 'Covalence is really hard.', id: 1},
+      {value: 'But it\'s also really fun!', id: 2}
+    ];
   }
 
   handleInputChange = (value) => {
@@ -26,6 +32,7 @@ class App extends Component {
         value={this.state.name}
         onChange={(e) => this.handleInputChange(e.target.value)} 
       />
+      <List items={this.items} />
       <Comment
         author={{ name: this.state.name, avatarUrl: 'https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/45500450_2097247793661341_7920028614194626560_n.jpg?_nc_cat=100&_nc_ht=scontent.fapa1-2.fna&oh=4d9829525619e260958d8fa729ac3857&oe=5CA846BF' }}
         text="Hi, this is what a husky begging looks like!"
